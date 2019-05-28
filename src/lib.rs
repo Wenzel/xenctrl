2947682ae50a9f1ffac411d70508cf6b11cb0ffe
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-05-28 22:58:00 +0200

    add new and close

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,25 @@
 extern crate xenctrl_sys;
 use std::ptr::{null_mut};
 
-struct Xc {
+pub struct Xc {
     handle: *mut xenctrl_sys::xc_interface,
 }
 
 impl Xc {
 
-    pub fn new(&mut self) {
-        self.handle = unsafe {
+    pub fn new() -> Xc {
+        let xc_handle = unsafe {
             let toto = xenctrl_sys::xc_interface_open(null_mut(), null_mut(), 0);
             toto
         };
+        let xc = Xc { handle: xc_handle };
+        xc
     }
-}
 
-// close
-impl Drop for Xc {
-
-    fn drop(&mut self) {
+    pub fn close(&mut self) {
         unsafe {
             xenctrl_sys::xc_interface_close(self.handle);
         };
+        self.handle = std::ptr::null_mut();
     }
 }

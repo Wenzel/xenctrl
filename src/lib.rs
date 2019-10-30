6a77becd217e3d00d1ffd55e7ca4a3ceedfe7f7b
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-10-30 10:21:05 +0100

    add domain_hvm_getcontext_partial

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,15 @@ extern crate xenctrl_sys;
 use std::{
     mem,
     ptr::{null_mut, NonNull},
+    ffi::c_void,
     convert::TryInto,
 };
 
 use xenctrl_sys::{
     xc_clear_last_error, xc_domain_maximum_gpfn, xc_domain_pause, xc_domain_unpause, xc_error_code,
     xc_get_last_error, xc_interface, xc_interface_close, xc_interface_open, xc_monitor_disable,
-    xc_monitor_enable, xentoollog_logger,
+    xc_monitor_enable, xentoollog_logger, xc_domain_hvm_getcontext_partial, hvm_hw_cpu,
+    __HVM_SAVE_TYPE_CPU,
 };
 
 use error::Error;
@@ -53,6 +55,22 @@ impl XenControl {
 
     pub fn default() -> Result<Self> {
         Self::new(None, None, 0)
+    }
+
+    pub fn domain_hvm_getcontext_partial(&self, domid: u32, vcpu: u16) -> Result<hvm_hw_cpu>{
+        let xc = self.handle.as_ptr();
+        let mut hvm_cpu: hvm_hw_cpu = unsafe { mem::MaybeUninit::<hvm_hw_cpu>::zeroed().assume_init() };
+        // cast to mut c_void*
+        let hvm_cpu_ptr = &mut hvm_cpu as *mut _ as *mut c_void;
+        // #define HVM_SAVE_CODE(_x) (sizeof (((struct __HVM_SAVE_TYPE_##_x *)(0))->c))
+        let hvm_save_type_cpu = unsafe { mem::MaybeUninit::<__HVM_SAVE_TYPE_CPU>::zeroed().assume_init() };
+        let hvm_save_code_cpu: u16 = mem::size_of_val(&hvm_save_type_cpu.c).try_into().unwrap();
+        let hvm_size: u32 = mem::size_of::<hvm_hw_cpu>().try_into().unwrap();
+        unsafe {
+            xc_clear_last_error(xc);
+            xc_domain_hvm_getcontext_partial(xc, domid, hvm_save_code_cpu, vcpu, hvm_cpu_ptr, hvm_size)
+        };
+        last_error!(xc, hvm_cpu)
     }
 
     pub fn monitor_enable(&mut self, domid: u32) -> Result<&PageInfo> {

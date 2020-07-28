8b79e3b8044fb353f33e40ca55a8f1162cffcb0f
arnabcs17b006 <cs17b006@iittp.ac.in>
2020-07-28 16:49:10 +0530

    rust bindings for xen events

--- src/libxenctrl.rs
+++ src/libxenctrl.rs
@@ -1,6 +1,6 @@
 use std::os::raw::{c_char, c_int, c_uint, c_void};
 
-use xenctrl_sys::{domid_t, xc_error, xc_interface, xen_pfn_t, xentoollog_logger};
+use xenctrl_sys::{domid_t, xc_error, xc_interface, xen_pfn_t, xenmem_access_t, xentoollog_logger};
 
 use libloading::{os::unix::Symbol as RawSymbol, Library, Symbol};
 use log::info;
@@ -47,6 +47,31 @@ type FnDomainMaximumGPFN =
     fn(xch: *mut xc_interface, domid: domid_t, gpfns: *mut xen_pfn_t) -> c_int;
 // xc_interface_close
 type FnInterfaceClose = fn(xch: *mut xc_interface) -> c_int;
+//xc_monitor_software_breakpoint
+type FnMonitorSoftwareBreakpoint = fn(xch: *mut xc_interface, domid: u32, enable: bool) -> c_int;
+//xc_monitor_mov_to_msr
+type FnMonitorMovToMsr =
+    fn(xch: *mut xc_interface, domain_id: domid_t, msr: u32, enable: bool) -> c_int;
+//xc_monitor_write_ctrlreg
+type FnMonitorWriteCtrlreg = fn(
+    xch: *mut xc_interface,
+    domain_id: domid_t,
+    index: u16,
+    enable: bool,
+    sync: bool,
+    onchangeonly: bool,
+) -> c_int;
+//xc_get_mem_access
+type FnGetMemAccess =
+    fn(xch: *mut xc_interface, domain_id: domid_t, pfn: u64, access: *mut xenmem_access_t) -> c_int;
+//xc_set_mem_access
+type FnSetMemAccess = fn(
+    xch: *mut xc_interface,
+    domain_id: domid_t,
+    access: xenmem_access_t,
+    first_pfn: u64,
+    nr: u32,
+) -> c_int;
 
 #[derive(Debug)]
 pub struct LibXenCtrl {
@@ -64,6 +89,11 @@ pub struct LibXenCtrl {
     pub domain_unpause: RawSymbol<FnDomainUnpause>,
     pub domain_maximum_gpfn: RawSymbol<FnDomainMaximumGPFN>,
     pub interface_close: RawSymbol<FnInterfaceClose>,
+    pub monitor_software_breakpoint: RawSymbol<FnMonitorSoftwareBreakpoint>,
+    pub monitor_mov_to_msr: RawSymbol<FnMonitorMovToMsr>,
+    pub monitor_write_ctrlreg: RawSymbol<FnMonitorWriteCtrlreg>,
+    pub get_mem_access: RawSymbol<FnGetMemAccess>,
+    pub set_mem_access: RawSymbol<FnSetMemAccess>,
 }
 
 impl LibXenCtrl {
@@ -107,6 +137,24 @@ impl LibXenCtrl {
         let domain_pause_sym: Symbol<FnDomainPause> = lib.get(b"xc_domain_pause\0").unwrap();
         let domain_pause = domain_pause_sym.into_raw();
 
+        let monitor_software_breakpoint_sym: Symbol<FnMonitorSoftwareBreakpoint> =
+            lib.get(b"xc_monitor_software_breakpoint\0").unwrap();
+        let monitor_software_breakpoint = monitor_software_breakpoint_sym.into_raw();
+
+        let monitor_mov_to_msr_sym: Symbol<FnMonitorMovToMsr> =
+            lib.get(b"xc_monitor_mov_to_msr\0").unwrap();
+        let monitor_mov_to_msr = monitor_mov_to_msr_sym.into_raw();
+
+        let monitor_write_ctrlreg_sym: Symbol<FnMonitorWriteCtrlreg> =
+            lib.get(b"xc_monitor_write_ctrlreg\0").unwrap();
+        let monitor_write_ctrlreg = monitor_write_ctrlreg_sym.into_raw();
+
+        let get_mem_access_sym: Symbol<FnGetMemAccess> = lib.get(b"xc_get_mem_access\0").unwrap();
+        let get_mem_access = get_mem_access_sym.into_raw();
+
+        let set_mem_access_sym: Symbol<FnSetMemAccess> = lib.get(b"xc_set_mem_access\0").unwrap();
+        let set_mem_access = set_mem_access_sym.into_raw();
+
         let domain_unpause_sym: Symbol<FnDomainUnpause> = lib.get(b"xc_domain_unpause\0").unwrap();
         let domain_unpause = domain_unpause_sym.into_raw();
 
@@ -129,6 +177,11 @@ impl LibXenCtrl {
             domain_hvm_setcontext,
             monitor_enable,
             monitor_disable,
+            monitor_software_breakpoint,
+            monitor_mov_to_msr,
+            monitor_write_ctrlreg,
+            get_mem_access,
+            set_mem_access,
             domain_pause,
             domain_unpause,
             domain_maximum_gpfn,
